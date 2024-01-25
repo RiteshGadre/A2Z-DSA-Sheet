7c1f269186a9aad5423a177c9eee1c315b5ff819
RiteshGadre <riteshgadre9@gmail.com>
2024-01-25 18:26:37 +0530

    upper bound problem solved

--- Binary Search/BS on 1D Array/upperBound.cpp
+++ Binary Search/BS on 1D Array/upperBound.cpp
@@ -1,20 +1,19 @@
 // Time Complexity: O(n)
 // Space Complexity: O(1)
 
-int lowerBound(vector<int> arr, int n, int x) {
-	// Write your code here
-	
-	int low=0, high= n-1;
+int upperBound(vector<int> &arr, int x, int n){
+	// Write your code here.
+	int low= 0, high= n-1;
 	int ans= n;
-	while(low<= high){
+	while(low<=high){
 		int mid= (low+high)/2;
-		if(arr[mid]>=x){
+		if(arr[mid]> x){
 			ans= mid;
 			high= mid-1;
 		}
 		else{
 			low= mid+1;
 		}
-	}
+	}	
 	return ans;
 }
