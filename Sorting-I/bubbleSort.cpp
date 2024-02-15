f0690e2a8a83c4bc57a1004366868c81f5dd9d5f
RiteshGadre <riteshgadre9@gmail.com>
2024-02-15 22:45:28 +0530

    bubble sort problem solved

---
+++ Sorting-I/bubbleSort.cpp
@@ -0,0 +1,23 @@
+void solve(){
+	int n;
+	cin >> n;
+	vi arr(n);
+	for(int i=0; i<n; i++){
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n-1; ++i)
+	{
+		for (int j = 1; j < n; ++j)
+		{
+			if(arr[j]< arr[j-1]) 
+				swap(arr[j], arr[j-1]);
+		}
+	}
+
+	for (int i = 0; i < n; ++i){
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
