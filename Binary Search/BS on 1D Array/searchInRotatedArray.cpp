3a9f71c4257bb0b16a5cf1e537f67c79004e341b
RiteshGadre <riteshgadre9@gmail.com>
2024-04-05 10:25:02 +0530

    Problem resolved

--- Binary Search/BS on 1D Array/searchInRotatedArray.cpp
+++ Binary Search/BS on 1D Array/searchInRotatedArray.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -19,3 +22,9 @@ public:
         return -1;
     }
 };
+
+int main(){
+    Solution s1;
+    s1.search({6,7,1,2,3,4,5}, 4);
+    return 0;
+}
