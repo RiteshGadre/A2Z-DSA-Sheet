fdb6b8c536fb4512381d608531f04ae3107ef3ab
RiteshGadre <riteshgadre9@gmail.com>
2024-04-12 19:16:46 +0530

    Problem 5 Solved

---
+++ Arrays/Medium/5. Print subarray with maxmum subarray.cpp
@@ -0,0 +1,16 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    // Write your code here
+    int left= 0, right= 0, result= 0;
+    int n= a.size();
+    long long sum= 0;
+
+    while(right < n){
+        sum+= a[right];
+        while(left <= right && sum > k){
+            sum-= a[left++];
+        }
+        if(sum== k) result= max(result, right-left+1);
+        right++;
+    }
+    return result;
+}
